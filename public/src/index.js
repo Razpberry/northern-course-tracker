7d43356abe4d66bc76c634f5a4eaa8418528e91f
Razpberry <nstakoun@gmail.com>
2025-01-27 23:04:39 -0500

    Added database form

--- public/src/index.js
+++ public/src/index.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 // initializeApp = require('firebase/app');
 import { getAnalytics, setAnalyticsCollectionEnabled } from "firebase/analytics";
 // getAnalytics = require('firebase/analytics');
-import { getFirestore, collection, doc, setDoc, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, doc, setDoc, getDocs, addDoc, updateDoc, arrayUnion } from 'firebase/firestore';
 // getFirestore = require('firebase/firestore');
 
 // Your web app's Firebase configuration
@@ -69,8 +69,9 @@ window.getStudents = () => {
             allIds.push({ ...doc.data(), id: doc.id})
           })
 
+          ids.sort()
+          allIds.sort()
 
-          console.log(ids)
           for (let i = 0; i < ids.length; i++) {
             for (let j = 0; j < allIds.length; j++) {
               let student = allIds[j]
@@ -93,4 +94,69 @@ window.getStudents = () => {
     .catch(err => {
       console.log(err.message)
     })  
+}
+
+window.onload = () => {
+  const addStudentForm = document.getElementById('addform')
+  let id = ''
+
+  addStudentForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let crs = [
+      addStudentForm.course1.value, 
+      addStudentForm.course2.value, 
+      addStudentForm.course3.value, 
+      addStudentForm.course4.value
+    ]
+
+    addDoc(colRefS, {
+      firstname: addStudentForm.firstname.value,
+      lastname: addStudentForm.lastname.value,
+      courses: crs
+    })
+    .then((docRef) => {
+      document.getElementById("formconfirmation").innerText = 'Submitted! Thank you!'
+      id = docRef.id
+
+      getDocs(colRefC)
+        .then((snapshot) => {
+          let allCrs = []
+          snapshot.docs.forEach((doc) => {
+            allCrs.push({ ...doc.data(), id: doc.id})
+          })
+
+          for (let i = 0; i < crs.length; i++) {
+            let found = false;
+
+            for (let j = 0; j < allCrs.length; j++) {
+              if (crs[i] == allCrs[j].id) {
+                found = true;
+              
+                let path = 'courses/' + crs[i]
+                let ref = doc(db, path)
+                updateDoc(ref, {
+                  students: arrayUnion(id)
+                })
+
+                break;
+              }
+            }
+            if (!found) {
+              let path = 'courses/' + crs[i]
+              let ref = doc(db, path)
+              setDoc(ref, {
+                students: [
+                  id
+                ]
+              })
+            }
+          }
+        })
+
+        addStudentForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+  })
 }
